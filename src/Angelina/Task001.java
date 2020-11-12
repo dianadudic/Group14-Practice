0733106067103904ad8d6d3a4a7157ee19144ca9
angelina788 <limura788@gmail.com>
2020-11-12 13:13:15 -0500

    added new line

--- src/Angelina/Task001.java
+++ src/Angelina/Task001.java
@@ -2,9 +2,5 @@ package Angelina;
 
 public class Task001 {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello Group 14!");
-        // everybody here???
-    }
+    //new line
 }
