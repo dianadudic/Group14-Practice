6a884f9705d042d11272cba5acae3e27116a6943
Sevara Isroilova <sevara.i.d@gmail.com>
2020-11-12 13:29:43 -0500

    Sevara's 4 commit

--- src/Sevara/Nov11_FirstPractice.java
+++ src/Sevara/Nov11_FirstPractice.java
@@ -8,7 +8,7 @@ public class Nov11_FirstPractice {
 // just pushed my first commit
         //pushing my second commit
         //typing one more comment
-
+//hey
 
         System.out.println();
     }}
