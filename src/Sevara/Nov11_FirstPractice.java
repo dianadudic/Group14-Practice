41c6e17afab6a9013c45165173d30d937d14c34c
Sevara Isroilova <sevara.i.d@gmail.com>
2020-11-12 12:56:27 -0500

    Sevara's 1 commit

---
+++ src/Sevara/Nov11_FirstPractice.java
@@ -0,0 +1,9 @@
+package Sevara;
+
+public class Nov11_FirstPractice {
+    public static void main(String[] args) {
+        System.out.println("Hi Group 14");
+        System.out.println("This is Sevara");
+        System.out.println("I have created my first practice Class under my name");
+
+    }}
